6d757b99671c6c051ff27df5c42929eb332b14d6
HeloisaGski <146874643+HeloisaGski@users.noreply.github.com>
2024-11-23 04:33:32 -0300

    Initial commit

---
+++ resources/views/pokemon/edit.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.base');
+@section('content')
+<form class="max-w-sm mx-auto" action="{{ url('pokemon/'.$pokemon->id) }}" method="POST">
+    @csrf
+    @method('PUT')
+        <div class="mb-5">
+        <label for="name" class="block mb-2 text-sm font-medium text-white dark:text-white">Name</label>
+        <input type="text" name="name" value="{{$pokemon->name}}" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+    </div>
+    <div class="mb-5">
+        <label for="type" class="block mb-2 text-sm font-medium text-white dark:text-white">Type</label>
+        <input type="text" name="type" value="{{$pokemon->type}}" id="type" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+    </div>
+    <div class="mb-5">
+        <label for="power" class="block mb-2 text-sm font-medium text-white dark:text-white">Power</label>
+        <input type="number" name="power" value="{{$pokemon->power}}" id="power" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+    </div>
+
+    <button type="submit" class="text-yellow-400 hover:text-white border border-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:outline-none focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-yellow-300 dark:text-yellow-300 dark:hover:text-white dark:hover:bg-yellow-400 dark:focus:ring-yellow-900">Update Pokemon</button>
+    </form>
+@endsection
+
+
---
+++ resources/views/pokemon/index.blade.php
@@ -0,0 +1,48 @@
+@extends('layouts.base')
+
+@section('content')
+<div class="max-w-screen-xl mx-auto p-5 sm:p-10 md:p-16">
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10">
+        @foreach($pokemon as $entity)
+        <div class="rounded overflow-hidden shadow-lg">
+            <div class="relative">
+                <img class="w-full"
+                    src="https://cdn.ome.lt/qyFc-3bXb4b863CxEXHN7_MhXvw=/770x0/smart/uploads/conteudo/fotos/Pokemon_Mewtwo.jpg" 
+                    alt="{{$entity->name}}">
+                <div
+                    class="hover:bg-transparent transition duration-300 absolute bottom-0 top-0 right-0 left-0 bg-gray-900 opacity-25">
+                </div>
+                <div
+                    class="absolute bottom-0 left-0 bg-white-600 px-4 py-2 text-white text-sm hover:bg-white hover:text-black transition duration-500 ease-in-out">
+                    {{$entity->type}}
+                </div>
+                <div
+                    class="text-sm absolute top-0 right-0 bg-white px-4 text-dark rounded-full h-16 w-16 flex flex-col items-center justify-center mt-3 mr-3 hover:bg-white hover:text-red-700 transition duration-500 ease-in-out">
+                    <span class="font-bold">{{$entity->power}}</span>
+                    <small>Power</small>
+                </div>
+            </div>
+            <div class="px-6 py-4">
+                <h5 class="font-semibold text-lg inline-block text-white">
+                    {{$entity->name}}
+                </h5>
+            </div>
+            <div class="px-6 py-4 flex justify-between">
+                <a href="{{ url('pokemon/'.$entity->id.'/edit') }}" 
+                   class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-4 py-2">
+                    Edit✏️
+                </a>
+                <form action="{{ url('pokemon/'.$entity->id) }}" method="POST" onsubmit="return confirm('Are you sure?');">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit"
+                        class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-4 py-2">
+                        Delete🗑️
+                    </button>
+                </form>
+            </div>
+        </div>
+        @endforeach
+    </div>
+</div>
+@endsection
