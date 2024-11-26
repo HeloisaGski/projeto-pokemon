2d3ad81db5a73c27534436501fc4e2bf4d2b2545
HeloisaGski <146874643+HeloisaGski@users.noreply.github.com>
2024-11-25 23:32:58 -0300

    .

--- resources/views/pokemon/edit.blade.php
+++ resources/views/pokemon/edit.blade.php
@@ -35,7 +35,7 @@
         </select>
     </div>
 
-    <button type="submit" class="text-yellow-400 hover:text-white border border-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:outline-none focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-yellow-300 dark:text-yellow-300 dark:hover:text-white dark:hover:bg-yellow-400 dark:focus:ring-yellow-900">Update Pokemon</button>
+    <button type="submit" class="text-white hover:text-white border border-red-900 hover:bg-red-900 focus:ring-4 focus:outline-none focus:ring-red-900 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-red-900 dark:text-red-900 dark:hover:text-white dark:hover:bg-red-900 dark:focus:ring-red-900">Update Pokemon</button>
     </form>
 @endsection
 
